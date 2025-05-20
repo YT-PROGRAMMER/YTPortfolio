16094277eb1617604b3d7ba0b673bc00e77d87f5
youssef <ytelmalwany@gmail.com>
2025-05-21 00:52:01 +0300

    updata youssef

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,6 +15,13 @@ const Contact = () => {
         </div>
         <Email />
       </div>
+        <footer>
+            <a href="https://t.me/yousseftarekd0"><i class="fa-brands fa-telegram"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100092417117506&amp;mibextid=ZbWKwL"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://wa.me/qr/MCMZGSHJATHOA1"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href="https://www.tiktok.com/@0_yt_programmer_8?_t=ZS-8MGuOBJqbg&amp;_r=1"><i class="fa-brands fa-tiktok"></i></a>
+            <a href="https://www.linkedin.com/in/yousseftarek~"><i class="fa-brands fa-linkedin"></i></a>
+        </footer>
     </div>
   );
 }
